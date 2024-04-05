51e5bcb728884e7088e741ceca56f1198d0a9191
spandandas98 <146106947+spandandas98@users.noreply.github.com>
2024-04-05 16:58:16 +0530

    added

--- fresh-farm-produce/src/Pages/ManageProduct.js
+++ fresh-farm-produce/src/Pages/ManageProduct.js
@@ -1,12 +1,65 @@
-import React from "react";
+import React, { useState } from 'react';
+import "./Style/manage_product.css";
 
-function ManageProduct() {
+const ManageProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [productName, setProductName] = useState('');
+  const [productPrice, setProductPrice] = useState('');
+
+  const handleProductNameChange = (e) => {
+    setProductName(e.target.value);
+  };
+
+  const handleProductPriceChange = (e) => {
+    setProductPrice(e.target.value);
+  };
+
+  const handleAddProduct = () => {
+    if (productName && productPrice) {
+      const newProduct = {
+        id: new Date().getTime(),
+        name: productName,
+        price: parseFloat(productPrice),
+      };
+      setProducts([...products, newProduct]);
+      setProductName('');
+      setProductPrice('');
+    }
+  };
+
+  const handleDeleteProduct = (id) => {
+    const updatedProducts = products.filter((product) => product.id !== id);
+    setProducts(updatedProducts);
+  };
+
   return (
     <div>
       <h2>Manage Products</h2>
-      {/* Add your management functionality here */}
+      <div className='manage_Product'>
+        <input
+          type="text"
+          placeholder="Product Name"
+          value={productName}
+          onChange={handleProductNameChange}
+        />
+        <input
+          type="number"
+          placeholder="Price"
+          value={productPrice}
+          onChange={handleProductPriceChange}
+        />
+        <button onClick={handleAddProduct}>Add Product</button>
+      </div>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - ${product.price}{' '}
+            <button onClick={() => handleDeleteProduct(product.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default ManageProduct;
+export default ManageProducts;
