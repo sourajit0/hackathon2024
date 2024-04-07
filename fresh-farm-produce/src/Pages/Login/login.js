325872a69373110e56f8de930084b7ac2d048557
Suvodeep28 <b.suvodeep30@gmail.com>
2024-04-07 12:13:33 +0530

    Suvodeep11

--- fresh-farm-produce/src/Pages/Login/login.js
+++ fresh-farm-produce/src/Pages/Login/login.js
@@ -1,24 +1,21 @@
-import React, { useState, useEffect } from "react";
+// Login.js
+import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 import { useCurrentUser } from "./CurrentUserContext";
 import HomePage from "../../homePage";
 import Signup from "../Signup/Signup";
+import Footer from "../../components/Footer"
+import "../Signup/Signup.css"
 import Navlog from "./nav";
-import "./loginStyle.css";
+import "./loginStyle.css"
+
 
 const Login = () => {
-  const { user, setUser } = useCurrentUser();
+  const { setUser } = useCurrentUser();
+  // const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [loginSuccess, setLoginSuccess] = useState(false);
-
-  useEffect(() => {
-    // Check if user is already authenticated from localStorage
-    const storedUser = localStorage.getItem("currentUser");
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-      setLoginSuccess(true); // Set login success flag
-    }
-  }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -31,13 +28,9 @@ const Login = () => {
         body: JSON.stringify({ username, password }),
       });
       const data = await response.json();
-
       if (data.success) {
         const { userId } = data;
-
         setUser(userId);
-
-        localStorage.setItem("currentUser", JSON.stringify(userId)); // Save user to localStorage
         setLoginSuccess(true); // Set login success flag
       } else {
         alert("Login failed. Please check your credentials.");
@@ -56,41 +49,40 @@ const Login = () => {
   // Render login form if not logged in
   return (
     <div>
-      <Navlog /> {/* Include the navbar component */}
-      <div className="container">
-        <div className="form-container">
-          <div className="signup-form">
-            <Signup />
-          </div>
-          <div className="login-form">
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="username">Username:</label>
-              <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
-              <br />
-              <br />
-              <label htmlFor="password">Password:</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <br />
-              <br />
-              <button type="submit">Login</button>
-            </form>
-          </div>
-        </div>
+    <Navlog />
+    <div className="container">
+      <div className="signup-form">
+        <Signup />
+      </div>
+      <div className="login-form">
+        <h2 style={{textAlign:"left"}}>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <br />
+          <br />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <br />
+          <br />
+          <button type="submit">Login</button>
+        </form>
       </div>
     </div>
+    <Footer/>
+  </div>
   );
 };
 
